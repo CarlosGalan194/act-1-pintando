6756bb12b066360dafe2fd3c5aade1edf629b69c
CarlosGalan194 <a01425194@tec.mx>
2026-09-17 17:55:11 -0600

    completanfo y rellenando el triangulo rectangulo con solo dos puntos

--- paint.py
+++ paint.py
@@ -57,9 +57,17 @@ def rectangle(start, end):
     end_fill()
 
 def triangle(start, end):
-    """Draw triangle from start to end."""
-    
+    "Draw triangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    goto(end.x, start.y)
+    goto(end.x, end.y)
+    goto(start.x, start.y)
+
+    end_fill()
 
 def tap(x, y):
     """Store starting point or draw shape."""
@@ -95,3 +103,4 @@ onkey(lambda: store('shape', circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
+
